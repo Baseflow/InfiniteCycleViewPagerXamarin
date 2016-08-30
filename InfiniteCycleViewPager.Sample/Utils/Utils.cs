e385b429f61e78b2eb3c4a3375a786738b5b7522
Martijn van Dijk <mhvdijk@gmail.com>
2016-08-30 15:12:39 +1000

    Fix images on viewpager

--- InfiniteCycleViewPager.Sample/Utils/Utils.cs
+++ InfiniteCycleViewPager.Sample/Utils/Utils.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using Android.Content;
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.Views;
 using Android.Widget;
 using Com.Bumptech.Glide;
+using Com.Bumptech.Glide.Load.Engine;
+using Com.Bumptech.Glide.Request;
+using Com.Bumptech.Glide.Request.Animation;
+using Com.Bumptech.Glide.Request.Target;
 
 namespace InfiniteCycleViewPager.Sample.Utils
 {
@@ -41,16 +46,40 @@ namespace InfiniteCycleViewPager.Sample.Utils
         }
     }
 
+    public class mOnImageClickListener : Java.Lang.Object, View.IOnClickListener
+    {
+        public void OnClick(View v)
+        {
+            //IntentHelper.OpenUrlInBrowser(view.Context, (string)view.GetTag(view.getId()));
+        }
+    }
+
+    public class RequestListener : Java.Lang.Object, IRequestListener
+    {
+        public bool OnException(Java.Lang.Exception p0, Java.Lang.Object p1, ITarget p2, bool p3)
+        {
+            return false;
+        }
+
+        public bool OnResourceReady(Java.Lang.Object resource, Java.Lang.Object model, ITarget target, bool isFromMemoryCache, bool isFirstResource)
+        {
+            var imageViewTarget = (ImageViewTarget)target;
+
+            return new DrawableCrossFadeFactory()
+                    .Build(isFromMemoryCache, isFirstResource)
+                    .Animate(
+                            new BitmapDrawable(
+                        ((View)imageViewTarget.View).Resources,
+                        //Cast to Bitmap is not valid
+                        (Bitmap)resource
+                            ),
+                            imageViewTarget
+                    );
+        }
+    }
+
     public static class AppUtils
     {
-
-        /* private static View.OnClickListener mOnImageClickListener = new View.OnClickListener() {
-         @Override
-         public void onClick(final View view)
-         {
-             IntentHelper.openUrlInBrowser(view.getContext(), (String)view.getTag(view.getId()));
-         }*/
-
         public static void SetupImage(Context context, ImageView imageView, LibraryObject libraryObject)
         {
             SetupImage(context, imageView, libraryObject, -1);
@@ -58,38 +87,16 @@ namespace InfiniteCycleViewPager.Sample.Utils
 
         public static void SetupImage(Context context, ImageView imageView, LibraryObject libraryObject, int position)
         {
-
             imageView.SetTag(imageView.Id, libraryObject.getUrl());
+            imageView.SetOnClickListener(new mOnImageClickListener());
 
-            //imageView.SetOnClickListener(mOnImageClickListener);
-            /*
+            //Will load the images, but is quite slow
             Glide.With(context)
-                    .Load(position == -1 ? libraryObject.getImage() : libraryObject.getImages()[position]).
-                    .asBitmap()
-                    .centerCrop()
-                    .listener(new RequestListener<String, Bitmap>() {
-                    @Override
-                        public boolean onException(final Exception e, final String model, final Target<Bitmap> target, final boolean isFirstResource)
-        {
-            return false;
-        }
-
-        @Override
-                    public boolean onResourceReady(final Bitmap resource, final String model, final Target<Bitmap> target, final boolean isFromMemoryCache, final boolean isFirstResource)
-        {
-            final ImageViewTarget imageViewTarget = (ImageViewTarget)target;
-            return new DrawableCrossFadeFactory<>()
-                    .build(isFromMemoryCache, isFirstResource)
-                    .animate(
-                            new BitmapDrawable(
-                                    imageViewTarget.getView().getResources(),
-                                    resource
-                            ),
-                            imageViewTarget
-                    );
-        }
-    })
-                .into(imageView);*/
+                    .Load(position == -1 ? libraryObject.getImage() : libraryObject.getImages()[position])
+                 .CenterCrop()
+                 .DiskCacheStrategy(DiskCacheStrategy.Source)
+                 //.Listener(new RequestListener())
+                 .Into(imageView);
         }
     }
 }
